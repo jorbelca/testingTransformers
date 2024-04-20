2da6e64c052bffd3ef7c44a21f888d65095983c7
jorbelca <76847923+jorbelca@users.noreply.github.com>
2024-04-20 18:06:55 +0200

    Add files via upload

---  TransformersJS/scripts/index.js
+++  TransformersJS/scripts/index.js
@@ -14,3 +14,30 @@ document.getElementById("btn").addEventListener("click", async () => {
     } with ${Math.ceil(result[0].score * 100)} % accuracy`;
   }
 });
+
+let image = document.getElementById("img");
+image.addEventListener("change", async () => {
+  if (image.files.length > 0) {
+    //readURL(image.files[0]).then((res) => console.log(res));
+    const classifier = await pipeline(
+      "image-classification",
+      "Xenova/vit-base-patch16-224"
+    );
+    let result = await classifier(await readURL(image.files[0]), { topk: 0 });
+    result.forEach((element) => {
+      if (element.score > 0.1) {
+        console.log(`${element.label} => ${element.score}`);
+      }
+    });
+  }
+});
+
+// convert file to a base64 url
+const readURL = (file) => {
+  return new Promise((res, rej) => {
+    const reader = new FileReader();
+    reader.onload = (e) => res(e.target.result);
+    reader.onerror = (e) => rej(e);
+    reader.readAsDataURL(file);
+  });
+};
