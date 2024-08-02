f2998c287095cdb37e4f884f581b6df5b510892c
j <76847923+jorbelca@users.noreply.github.com>
2024-08-02 19:16:52 +0200

    cambios para la version de produccion de vercel

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,12 @@
 export let apiKey;
 
-if (window.location.hostname === "jorbelca.github.io") {
-  import("./config.production.js").then((module) => {
-    apiKey = module.Key;
-  });
+if (window.location.hostname === "testing-transformers.vercel.app") {
+  fetch("/api/getApiKey")
+    .then((response) => response.json())
+    .then((data) => {
+      apiKey = data.apiKey;
+    })
+    .catch((error) => console.error("Error fetching API key:", error));
 } else {
   import("../key.js").then((module) => {
     apiKey = module.Key;
