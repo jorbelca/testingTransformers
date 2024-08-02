730ca09b425abedf5b666768caf8086c1b4194d4
j <76847923+jorbelca@users.noreply.github.com>
2024-08-02 19:52:30 +0200

    cambios para la version de produccion

--- api/getApiKey.js
+++ api/getApiKey.js
@@ -1,9 +1,11 @@
 export default function handler(req, res) {
-    const apiKey = process.env.MY_API_KEY;
-  
-    if (!apiKey) {
-      return res.status(500).json({ error: 'API key not set in environment variables' });
-    }
-  
-    res.status(200).json({ apiKey });
+  const apiKey = process.env.KEY;
+
+  if (!apiKey) {
+    return res
+      .status(500)
+      .json({ error: "API key not set in environment variables" });
   }
+
+  res.status(200).json({ apiKey });
+}
